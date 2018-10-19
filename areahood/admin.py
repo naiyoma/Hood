df7d11329edf02669676181e2a4ca9b92f9d5107
naiyoma <lankas.aurelia@gmail.com>
2018-10-19 15:53:13 +0300

    registering tables

--- areahood/admin.py
+++ areahood/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Profile,Image,Comment
+from .models import Profile,Post,Neighbourhood
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Image)
-admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(Neighbourhood)
