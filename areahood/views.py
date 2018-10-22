877fbbc3f0317652221460f0dd368c619749c321
naiyoma <lankas.aurelia@gmail.com>
2018-10-22 16:51:15 +0300

    login in required

--- areahood/views.py
+++ areahood/views.py
@@ -4,17 +4,9 @@ from django.contrib.auth import authenticate,login
 from .forms import ProfileForm,PostForm,BusinessForm,CommunityForm
 from django.http import HttpResponse
 import datetime as dt
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.utils.encoding import force_bytes, force_text
-# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-# from django.template.loader import render_to_string
-# from .tokens import account_activation_token
-# from django.contrib.auth.models import User
-# from django.core.mail import EmailMessage
-# import datetime as dt
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login
 
-#profile views
+    
 def display(request):
     images = Profile.objects.all() 
     try:
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -129,6 +129,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
-LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'display'
+LOGOUT_REDIRECT_URL = 'auth_login'
